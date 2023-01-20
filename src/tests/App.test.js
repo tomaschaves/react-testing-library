7ef00473b37f4a0cf7dbf5d9c46e17938e2270c6
Tomás Chaves <tomcchaves@gmail.com>
2023-01-20 16:04:21 -0300

    Requisito 1 lint.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
